0add2cd539da1d2e847f32435a1a2d1d8760cb30
Ankit Kushwaha <ankitkshwh003@gmail.com>
2025-05-29 13:23:08 +0530

    Switch to Serverless mode

--- api/index.js
+++ api/index.js
@@ -3,20 +3,19 @@ dotenv.config();
 
 import { bot } from '../lib/bot/telegraf.js';
 
-// export default async (req, res) => {
-//   try {
-//     await bot.handleUpdate(req.body, res);
-//   } catch (err) {
-//     console.error('Error handling update:', err);
-//     res.status(500).send('Internal Server Error');
-//   }
-// };
+export default async (req, res) => {
+  try {
+    await bot.handleUpdate(req.body, res);
+  } catch (err) {
+    console.error('Error handling update:', err);
+    res.status(500).send('Internal Server Error');
+  }
+};
 
-(async () => {
-  // Launch in polling mode instead of webhook
-  await bot.launch();
-  console.log('🤖 Bot is up and running in polling mode');
-})();
+// // Run local
+// (async () => {
+//   await bot.launch();
+// })();
 
 // Graceful shutdown
 ['SIGINT', 'SIGTERM'].forEach(sig =>
